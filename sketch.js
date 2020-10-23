07507daf50f97bc214fef72b157e1b78de276842
chaturawhitehat20 <chaturavenkat2007@gmail.com>
2020-10-23 16:55:42 +0530

    added

--- sketch.js
+++ sketch.js
@@ -1,11 +1,13 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint=Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
-
+var constrainedLog;
+var chain;
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -33,9 +35,13 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
-
+    
+    constrainedLog = new Log(230,180,80,PI/2)
     bird = new Bird(100,100);
 
+    chain = new Chain(bird.body,constrainedLog.body);
+
+    
 }
 
 function draw(){
@@ -60,5 +66,9 @@ function draw(){
     log5.display();
 
     bird.display();
+    constrainedLog.display();
     platform.display();
+    
+    chain.display();
+     
 }
